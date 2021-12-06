4e6805e6be315c6286546b2169c1edbb461a9e5b
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-12-06 18:06:14 +0000

    fetched api

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../Card/Card";
-import beers from "../../data/beers";
 import "./CardList.scss";
 
 const CardList = () => {
-  const cardListJSX = beers.map((beer) => {
+  const [beerArray, setBeerArray] = useState ([]);
+
+  useEffect(() => {
+    const URL = `https://api.punkapi.com/v2/beers`;
+    fetch(URL)
+    .then(response => {
+      return response.json();
+    })
+    .then(beerObject => {
+      console.log(beerObject)
+      setBeerArray(beerObject);
+    })
+  }, [])
+
+  const cardListJSX = beerArray.map((beer) => {
     return <Card key={beer.id} name={beer.name} imageURL={beer.image_url} brewDate={beer.first_brewed} alcoholStrength={beer.abv}/>
   })
 
