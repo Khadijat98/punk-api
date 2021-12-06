3b83005f7ce828d92a1a7a3f1e68e536f5204740
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-12-06 18:36:41 +0000

    added search input and header for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
-
+  
   return <nav className="navbar">
-    <p>Nav works</p>
+    <h1 className="navbar__heading">Punk API</h1>
+    <label htmlFor="search-term"></label>
+    <input className="navbar__input" type="text" name="search-term" placeholder="Search..."/>
   </nav>
 }
 
