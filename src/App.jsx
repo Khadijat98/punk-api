57be737a86c292e882776b0fad4ff6aff35c2636
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-12-08 19:05:17 +0000

    trying (and failing) to add third filter function

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,30 @@ import { useState, useEffect } from 'react';
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [beerArray, setBeerArray] = useState([]);
-  const [URLaddition, setURLaddition] = useState("")
+  const [URLaddition, setURLaddition] = useState("");
 
   const checkOne = (event) => {
-    const isCheckedBoxOne = event.target.checked;
-    if (isCheckedBoxOne) {
-      setURLaddition("abv_gt=6")
-    } else {
-      setURLaddition("")
-    };
+    const isCheckBoxOne = event.target.checked;
+    isCheckBoxOne ? setURLaddition("abv_gt=6") : setURLaddition("");
   }
 
   const checkTwo = (event) => {
-    const isCheckedBoxTwo = event.target.checked;
-    if (isCheckedBoxTwo) {
-      setURLaddition("brewed_before=01-2010")
-    } else {
-      setURLaddition("")
-    };
+    const isCheckBoxTwo = event.target.checked;
+    isCheckBoxTwo ? setURLaddition("brewed_before=01-2010") : setURLaddition("");
   }
 
   const checkThree = (event) => {
-    const isCheckedBoxThree = event.target.checked;
-    console.log(isCheckedBoxThree);
+    const isCheckBoxThree = event.target.checked;
+    console.log(isCheckBoxThree);
 
-    if (isCheckedBoxThree) {
-      const acidicBeer = beerArray.filter(beer => {
-        if (beer.ph < 4) {
-          setURLaddition(beer.ph)
-        }
-      //  console.log(beer.ph);
-      return (beer.ph < 4);
-     })
-     return acidicBeer;
+    if (isCheckBoxThree) {
+      const filteredByBeerPh = beerArray.filter(beer => {
+        const acidicBeer = (beer.ph < 4)
+        setURLaddition(beer.ph)
+        return acidicBeer;
+      })
+
+      return filteredByBeerPh;
     } 
   }
 
