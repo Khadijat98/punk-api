41c8c81a637b244adcda0cf401b545326c810e00
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-12-13 10:07:54 +0000

    removed commented out code

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,9 +7,6 @@ const Card = (props) => {
   return <div className="beer-card">
     <img src={imageURL} alt={name} className="beer-card__image"/>
     <br />
-    
-      {/* <div className="beer-card__header">
-      </div> */}
       <div className="beer-card__text">
         <h3 className="beer-card__text--name">{name}</h3>
         <p className="beer-card__text--date">Brew Date: {brewDate}</p>
